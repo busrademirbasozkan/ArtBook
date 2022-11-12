886efcd3ecf50b1aac5f387d88ba9838aaa31e0c
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-11-12 18:33:04 +0300

    Kamera yada kullanıcı kütüphanesine erişebilmek için gerekli fonksiyonlar eklendi.

--- ArtBook/Controller/SecondVC.swift
+++ ArtBook/Controller/SecondVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SecondVC: UIViewController {
+class SecondVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameTextField: UITextField!
@@ -17,11 +17,41 @@ class SecondVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Klavyeyi kapatmak için
+        
+        
+        
+        //Recognizer
+        //klavyeyi kapatmak için
         let gesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(gesture)
+        
+        //Görsel seçmek için
+        imageView.isUserInteractionEnabled = true
+        let imageGesture = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageGesture)
+        
 }
     
+    //Resme tıklanınca resim seçmek için gerekli işlemleri tanımlamak içim
+    @objc func selectImage(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    
+    //görseli seçince ne olacağını tanımalamalıyız
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+        
+    }
+    
+    
+    //Klavyeyi ekranda herhangi bir yere basıldıpında kapatmak için
     @objc func hideKeyboard(){
         view.endEditing(true)
         
