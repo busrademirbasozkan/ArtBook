b1c530f32fabb496da56b0f683832cec9c269c67
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-11-14 14:18:09 +0300

    Comment satırları güncellendi.

--- ArtBook/Controller/SecondVC.swift
+++ ArtBook/Controller/SecondVC.swift
@@ -24,7 +24,7 @@ class SecondVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
         //Recognizer
         //klavyeyi kapatmak için
         let gesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-        view.addGestureRecognizer(gesture)
+        view.addGestureRecognizer(gesture) //view olduğunda ekranda herhangi bir yere bastığımızda recognizerın çalışmasını sağlar.
         
         //Görsel seçmek için
         imageView.isUserInteractionEnabled = true
@@ -35,10 +35,11 @@ class SecondVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     
     //Resme tıklanınca resim seçmek için gerekli işlemleri tanımlamak içim
     @objc func selectImage(){
+        //UIImagePickerController kullanıcının kamerasına yada medya kütüphanesine erişebilmemizi saplayan bir sınıf
         let picker = UIImagePickerController()
-        picker.delegate = self
-        picker.sourceType = .photoLibrary
-        picker.allowsEditing = true
+        picker.delegate = self // Bu işlemler için gerekli protokoller eklenmeli ve yetki viewcontrollera verilmeli
+        picker.sourceType = .photoLibrary // camera yada photolibrary seçebilir.
+        picker.allowsEditing = true // zoomlamak gibi edit yapmaya izin verme yada vermeme için
         present(picker, animated: true, completion: nil)
         
     }
@@ -46,36 +47,37 @@ class SecondVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     
     //görseli seçince ne olacağını tanımalamalıyız
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        imageView.image = info[.originalImage] as? UIImage
-        self.dismiss(animated: true)
-        
+        imageView.image = info[.originalImage] as? UIImage // seçilen resmi original yada editlenmiş olarak mı kaydedeceğimizi seçeriz. Casting yaparak gerçekleşitirilir çünkü kullanıcı fotoğraf seçmeyebilir. Böyle bir durumda sıkıntı yaşanmaması için
+        self.dismiss(animated: true) // açılan pickerın kapatılabilmesi için
     }
     
     
     //Klavyeyi ekranda herhangi bir yere basıldıpında kapatmak için
     @objc func hideKeyboard(){
-        view.endEditing(true)
+        view.endEditing(true) // view içinde yapılan değişiklikleri bitiriyor.
         
     }
     
     @IBAction func saveButton(_ sender: Any) {
         
+        //Coredatada verileri kaydedebilmek için contexte erişebilmemiz gerekli. Contexte erişebilmek için AppDelegate bir değişken olarak tanımlanmalı.
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
+        let context = appDelegate.persistentContainer.viewContext // AppDelegate içindeki contexte erişebildim.
         
-        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context) // contextin içine ne ekleyeceğimizi belirlememiz gerekir. Burda daha önce coredata da tanımladığımız entityi seçeriz.
         
-        //Attributes
+        //Entitydeki attributeları ilgili yerlere tanımlarız
         newPainting.setValue(nameTextField.text!, forKey: "name")
         newPainting.setValue(artistTextField.text!, forKey: "artist")
-        if let year = Int(yearTextField.text!){
+        if let year = Int(yearTextField.text!) { // yearın int olarak kaydedilmesi sağlanmalı
             newPainting.setValue(year, forKey: "year")
         }
-        newPainting.setValue(UUID(), forKey: "id")
+        newPainting.setValue(UUID(), forKey: "id") // swift bizim için bir id oluşturup kaydedecek.
         //image in data olarak çevrilmesi
-        let data = imageView.image?.jpegData(compressionQuality: 0.5)
+        let data = imageView.image?.jpegData(compressionQuality: 0.5) //compressionQuality resmin % kaçını alacağımızı belirtmemizi istiyor.
         newPainting.setValue(data, forKey: "image")
         
+        // context'i kaydederiz
         do{
             try context.save()
             print("success")
--- ArtBook/Controller/ViewController.swift
+++ ArtBook/Controller/ViewController.swift
@@ -6,23 +6,49 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var idArray = [UUID]()
+    var nameArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // + tuşunu eklemek için
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addClicked))
-        
-        
-        
     }
 
     @objc func addClicked(){
         performSegue(withIdentifier: "SecondVC", sender: nil)
+    }
+    
+    //Bu fonksiyon altında coredatadan verilerimizi çekeceğiz.
+    func getData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        //fetch ile dataları çekme
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        
+    }
+    
+    //TableView için func
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "text"
+        return cell
     }
 
 }
